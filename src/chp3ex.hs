07643ea5a9ca63cd5328db7ff3bf4a20348f2365
Mike <matroxmike@icloud.com>
2018-08-21 22:35:21 +1000

    types

--- src/chp3ex.hs
+++ src/chp3ex.hs
@@ -1,15 +1,22 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
 module Chp3Ex where
 
+addBang :: [Char] -> [Char]
 addBang str = str ++ "!"
 
+getFifth :: [a] -> a
 getFifth str = str !! 4
 
+dropNine :: [a] -> [a]
 dropNine str = drop 9 str
 
+thirdLetter :: [a] -> a
 thirdLetter str = str !! 2
 
+letterIndex :: Int -> Char
 letterIndex x = "Curry is awesome!" !! x
 
+rvrs :: [a] -> [a]
 rvrs str = awesome ++ is ++ curry
   where
     curry = take 5 str
